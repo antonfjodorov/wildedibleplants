51164993b36f64e1b9bfd414a61e39cdf63a4a35
Anton Fjodorov <antonfjodorov@hotmail.com>
2016-08-31 19:52:26 +0200

    -footer: darker bg for contrast -refactored language switcher -loading text changed -about section text and structure updated -scrolling to about section from header

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
+	// Set language
+	initLngClickers();
 	changeLng('en');
 	hideOtherLanguagesThan('en');
 
+	// enable smooth scrolling
+	initSmoothScroll();
+
 	// Get spreadsheet data
-	initTabletop(function(){
-	});
+	initTabletop(function(){});
 });
 /**
  * 1. change lng in the .cover
  * 2. change lng in the rest of the document
  */
 function changeLng(lng) {
-	$cover=$('.cover');
 	$('.cover .plant').each(function (i,el){
 		$el=$(el);
 		var id=el.attributes['dataId'].value;
@@ -20,10 +23,23 @@ function changeLng(lng) {
 		$el.find('h2').text(name);
 		$el.find('p').text(text);
 	});
-
+	
 	hideOtherLanguagesThan(lng);
 }
 function hideOtherLanguagesThan(lng){
 	$('.lng').hide();
 	$('.lng.'+lng).show();
+}
+function initSmoothScroll(){
+	$(".scroll").click(function (e){
+		var target=this.href.split('#')[1];
+		var top = $('#'+target).offset().top;
+		$('html,body').animate({ scrollTop:top }, 750, 'swing');
+	});
+}
+function initLngClickers(){
+	$('.lngClicker').click(function (e){
+		var lng = e.currentTarget.attributes['dataLng'].value;
+		changeLng(lng);
+	});
 }
